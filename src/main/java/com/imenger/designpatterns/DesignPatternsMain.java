801ba2d7ed9c37d4c75b02f3a5a8024abb32da43
meng <lvym@huazhenit.com>
2018-11-08 15:25:06 +0800

    Initial commit

---
+++ src/main/java/com/imenger/designpatterns/DesignPatternsMain.java
@@ -0,0 +1,12 @@
+package com.imenger.designpatterns;
+
+/**
+ * @author lvyimeng
+ * @version V1.0
+ * @Title: DesignPatterns
+ * @Description: Java设计模式
+ * @date: 2018/11/8 15:20
+ */
+public class DesignPatternsMain {
+
+}
